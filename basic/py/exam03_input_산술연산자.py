086b936a196dd066c187316228774d9933b6d28c
eunjin90 <eunjin900808@gmail.com>
2026-03-28 02:14:16 +0900

    Add files via upload

---
+++ basic/py/exam03_input_산술연산자.py
@@ -0,0 +1,19 @@
+#input() : 키보드 입력받는 함수
+num1 = input('첫번째 정수를 입력하세요')
+num2 = input('두번째 정수를 입력하세요')
+
+# input()의 특성: 무조건 결과를 문자열(str) 형태로 내뱉는다.
+#int(), float(), str() : type 변환함수
+
+print(num1, type(num1))
+num1 = int(num1)
+num2 = int(num2)
+
+print('num1 :', num1, 'num2',num2)
+print('두 정수의 합 : ',num1+num2)
+print('두 정수의 차 : ',num1-num2)
+print('두 정수의 곱 : ',num1*num2)
+print('두 정수의 나눗셈 : ',num1/num2)
+print('두 정수의 몫 : ',num1//num2)
+print('두 정수의 나머지 : ',num1%num2)
+
